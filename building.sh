73c1b024332d3071db1a7a4f2bf43e8108dc5767
François Garillot <francois@garillot.net>
2014-04-29 16:01:49 +0200

    bumping scala version

--- building.sh
+++ building.sh
@@ -31,11 +31,11 @@ pushd $TMPDIR
 wget https://github.com/scala/scala-dist/archive/master.zip
 unzip master.zip && rm master.zip
 mv scala-dist-master scala-dist
-SCALA_211_DIR=scala-dist/target/universal/scala-2.11.0-SNAPSHOT
+SCALA_211_DIR=scala-dist/target/universal/scala-2.11.1-SNAPSHOT
 export PATH=`pwd`/$SCALA_211_DIR/bin:$PATH
 cd scala-dist
-sbt-0.13.0 'set version := "2.11.0-SNAPSHOT"' 'set resolvers += "private-repo" at "http://private-repo.typesafe.com/typesafe/scala-release-temp/"' universal:package-bin
-cd target/universal && unzip scala-2.11.0-SNAPSHOT.zip
+sbt-0.13.0 'set version := "2.11.1-SNAPSHOT"' 'set resolvers += "private-repo" at "http://private-repo.typesafe.com/typesafe/scala-release-temp/"' universal:package-bin
+cd target/universal && unzip scala-2.11.1-SNAPSHOT.zip
 popd
 
 # The new process: just find the latest file from the download page
@@ -43,7 +43,7 @@ popd
 # # this depends on the antechronological listing on that page. So be it
 # pushd $TMPDIR
 # SCALA_NIGHTLY_NAME=$(wget -q -O -
-#     http://www.scala-lang.org/files/archive/nightly/distributions/ |  perl -lne 'if (/scala-2.11.0[^\"]*?.zip/){print $&; exit}' )
+#     http://www.scala-lang.org/files/archive/nightly/distributions/ |  perl -lne 'if (/scala-2.11.1[^\"]*?.zip/){print $&; exit}' )
 # wget -q "http://www.scala-lang.org/files/archive/nightly/distributions/$SCALA_NIGHTLY_NAME"
 # unzip $SCALA_NIGHTLY_NAME && rm $SCALA_NIGHTLY_NAME
 # SCALA_211_DIR=${SCALA_NIGHTLY_NAME%.zip}
@@ -52,8 +52,8 @@ popd
 # popd
 
 # Check : now this precise fresh scala 2.11 should be on the path
-GREPEE=$($TMPDIR/$SCALA_211_DIR/bin/scala -version 2>&1 |grep -Eoe "2.11.0[^\ ]*")
-if [[ $(scala -version 2>&1 | grep -Eoe "2.11.0[^\ ]*") != "$GREPEE" ]]; then echo "Couldn't put scala on path" && exit 2; fi
+GREPEE=$($TMPDIR/$SCALA_211_DIR/bin/scala -version 2>&1 |grep -Eoe "2.11.1[^\ ]*")
+if [[ $(scala -version 2>&1 | grep -Eoe "2.11.1[^\ ]*") != "$GREPEE" ]]; then echo "Couldn't put scala on path" && exit 2; fi
 
 # protect the nightly and instrument it with a 2.10 library
 SCALA_VERSION="2.10.4-RC2"
